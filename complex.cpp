1cea457ebf6c7a4f33ef5dfe4083f54d9491d903
Dariusz <vanchesco@icloud.com>
2020-05-28 17:37:00 +0200

    poprawka

--- complex.cpp
+++ complex.cpp
@@ -1,9 +1,7 @@
 #include "complex.h"
 
 klasa2::klasa2(void) : a(0), b(0) {};
-
 klasa2::klasa2(int a, int b) : a(a), b(b) {};
-
 klasa2::klasa2(const klasa1& k) : a(k.r), b(k.e)
 {
 	std::cout << "cpnv\n\n"; 
@@ -11,5 +9,4 @@ klasa2::klasa2(const klasa1& k) : a(k.r), b(k.e)
 
 
 klasa1::klasa1(void) : r(0), e(0) {};
-
 klasa1::klasa1(int r, int e) : r(r), e(e) {};

821cfb6bfecf6490dede80e410dc17879e9fe373
vanchesco <vanchesco@icloud.com>
2020-05-29 00:07:13 +0200

    Drugi konstruktor konwertujacy (nie dziala)

--- complex.cpp
+++ complex.cpp
@@ -4,9 +4,13 @@ klasa2::klasa2(void) : a(0), b(0) {};
 klasa2::klasa2(int a, int b) : a(a), b(b) {};
 klasa2::klasa2(const klasa1& k) : a(k.r), b(k.e)
 {
-	std::cout << "cpnv\n\n"; 
+	std::cout << "conv\n\n"; 
 };
 
 
 klasa1::klasa1(void) : r(0), e(0) {};
-klasa1::klasa1(int r, int e) : r(r), e(e) {};
+klasa1::klasa1(int r, int e) : r(r), e(e) {}
+klasa1::klasa1(const klasa2& k) : r(k.a), e(k.b)
+{
+	std::cout << "conv\n\n";
+};
--- complex.h
+++ complex.h
@@ -1,26 +1,30 @@
 #pragma once
 #include <iostream>
 
-class klasa1;
+class klasa2;
+
+
+class klasa1
+{
+public:
+	int r = 23, e = 54;
+
+	klasa1(void);
+	klasa1(int, int);
+	klasa1(const klasa2& k);
+
+	friend klasa2::klasa2(const klasa1& k);
+};
 
 class klasa2
 {
 public:
 
-    int a = 6, b = 5;
+	int a = 6, b = 5;
 
-    klasa2(void);
-    klasa2(int, int);
-    klasa2(const klasa1& k);
-};
+	klasa2(void);
+	klasa2(int, int);
+	klasa2(const klasa1& k);
 
-class klasa1
-{
-public:
-    int r = 23, e = 54;
-
-    klasa1(void);
-    klasa1(int, int);
-
-    friend klasa2::klasa2(const klasa1& k);
+	friend klasa1::klasa1(const klasa2& k);
 };
